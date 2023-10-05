723cf96785d446e385878aaf8a695d730fda3246
Ildar1994 <86912735+Ildar1994@users.noreply.github.com>
2023-10-05 22:28:52 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from selene import browser
 
 @pytest.fixture(scope='function', autouse='True')
 def browser_managment():
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_height = '1200'
     browser.config.window_width = '1800'
     yield
